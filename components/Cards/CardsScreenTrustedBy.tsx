425c7e35461892fa5b367895037fd58b21094268
_YOUSAN_NIM <ptolemaios.ong@gmail.com>
2024-08-06 09:12:48 +0700

    morning

--- components/Cards/CardsScreenTrustedBy.tsx
+++ components/Cards/CardsScreenTrustedBy.tsx
@@ -3,11 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { CardProps } from '@/types'
 
 const VibrantPrimaryFill_CardCol_1 = {
-<<<<<<< Updated upstream
     style: 'w-full  bg-grayDefaultDark-500 rounded-[20px] py-8 px-4 bg-opacity-50 text-white',
-=======
-    style: 'w-full h-[40ch] bg-grayDefaultDark-500 rounded-[20px] pl-10 pr-2 py-8 bg-opacity-50 text-white',
->>>>>>> Stashed changes
 }
 
 
